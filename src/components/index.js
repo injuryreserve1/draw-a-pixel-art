1e5692563db352c0bde432f7e43bb2d62d83f3cf
Ghost_Hardware <125590745+injuryreserve1@users.noreply.github.com>
2025-08-02 13:16:42 +0500

    idk

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 import { Canvas, scale } from "./ui/Canvas.js";
 import { ColorPicker } from "./ui/ColorPicker.js";
 import { Tools } from "./ui/Tools.js";
-import { ActionButton } from "./ui/ActionButton.js";
 
-export { Canvas, ColorPicker, Tools, ActionButton, scale };
+export { Canvas, ColorPicker, Tools, scale };
